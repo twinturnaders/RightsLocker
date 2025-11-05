66b0ffab437662643b644518bb1053ff32dd65a1
Brina Bright <bsonju1@wgu.edu>
2025-11-05 09:48:22 -0700

    changes to sharelink- pdf form fix on null users modifications to shell on frontend

--- frontend/src/app/features/evidence/evidence-page/evidence-page.component.ts
+++ frontend/src/app/features/evidence/evidence-page/evidence-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { NgIf } from '@angular/common';
 import { Evidence, EvidenceApi } from '../../../core/evidence.service';
-import { EvidenceUploadComponent } from '../evidence-upload/evidence-upload.component';
+
 import { EvidenceListComponent } from '../evidence-list/evidence-list.component';
 import { EvidenceDetailComponent} from '../evidence-detail/evidence-detail.component';
 import {ConvertComponent} from '../convert/convert.component';
