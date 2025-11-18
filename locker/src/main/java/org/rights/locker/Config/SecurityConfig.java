12cb11157710f5fc3431f25b12b03fb324268345
Brina Bright <bsonju1@wgu.edu>
2025-11-18 16:34:00 -0700

    it would help to import the evidence-detail and list components

--- locker/src/main/java/org/rights/locker/Config/SecurityConfig.java
+++ locker/src/main/java/org/rights/locker/Config/SecurityConfig.java
@@ -8,9 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -23,9 +21,7 @@ import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
-
 public class SecurityConfig {
-
 
     @Bean
     JwtAuthenticationFilter jwtAuthenticationFilter(JwtService jwtService, AppUserRepo userRepo) {
@@ -41,12 +37,11 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         // auth (public)
                         .requestMatchers("/api/auth/**").permitAll()
+
+                        .requestMatchers(HttpMethod.GET, "/api/evidence", "/api/evidence/**").authenticated()
+
+                        // anonymous upload only these 2
                         .requestMatchers("/api/evidence/presign-upload", "/api/evidence/finalize").permitAll()
-                        .requestMatchers("/api/evidence", "/api/evidence/**").authenticated()
-
-
-
-
 
                         // public share links
                         .requestMatchers("/api/share/**").permitAll()
@@ -65,11 +60,10 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         var cfg = new CorsConfiguration();
-        cfg.setAllowedOrigins(List.of("*", "http://localhost:4200", "http://127.0.0.1:30082", "http://127.0.0.1:30080", "http://127.0.0.1:30081", "http://127.0.0.1:8080"));
+        cfg.setAllowedOrigins(List.of("*"));
         cfg.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         cfg.setAllowedHeaders(List.of("*"));
         cfg.setAllowCredentials(false);
-        cfg.setAllowCredentials(true);
         var source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", cfg);
         return source;
